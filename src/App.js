7bf19688789c509c2cec23a63d73339e6ccabfd4
shubhamSP2198 <84079792+shubhamSP2198@users.noreply.github.com>
2021-10-24 20:24:25 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import MovieGrid from './components/MovieGrid'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <MovieGrid/>
+      </div>
+    )
+  }
+}
